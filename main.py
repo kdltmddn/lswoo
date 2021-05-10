3c3d21622ff53ae62bc644be710fec663b2e8f96
kdltmddn <82090311+kdltmddn@users.noreply.github.com>
2021-05-11 01:52:38 +0900

    디폴트 변경 목록

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# 샘플 Python 스크립트입니다.
+class cookie:
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# Shift 두 번을(를) 눌러 클래스, 파일, 도구 창, 액션 및 설정에서 전체 검색합니다.
+	def __init__(self, name, grade, level, skillname):
+		self.name = name
+		self.grade = grade
+		self.level = level
+		self.skillname = skillname
+
+	def info(self):
+		print(f'이름 :  {self.name}')
+		print(f'등급 : {self.grade}')
+		print(f'레벨 : {self.level}')
+		print(f'스킬 이름 : {self.skillname}')
+		print()
 
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+kingdom1 = cookie("용감한 쿠키", "COMMON", 18, "공격")
+kingdom2 = cookie("당근맛 쿠키", "RARE", 35, "치명타 공격")
+kingdom3 = cookie("라떼맛 쿠키", "EPIC", 44, "침묵")
+kingdom4 = cookie("퓨어바닐라 쿠키", "ANCIENT", 56, "회복")
+
+kingdom1.info()
+kingdom2.info()
+kingdom3.info()
+kingdom4.info()
 
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 확인
+
+
---
+++ practice.md
@@ -0,0 +1,4 @@
+![경북대학교](https://upload.wikimedia.org/wikipedia/commons/6/6b/Knuemblem00.jpg)
+  
+  
+
