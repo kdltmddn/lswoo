1fa6c3a12490644a43403a3fa98da5a3b2668bbd
kdltmddn <82090311+kdltmddn@users.noreply.github.com>
2021-05-18 01:21:15 +0900

    디폴트 변경 목록

---
+++ 2021113625.py
@@ -0,0 +1,5 @@
+num = 0
+while num < 3:
+    print(f'이상해씨가 {num}일째 씨 뿌리는 중')
+    num = num + 1
+    print("다 심었다!")
--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-class cookie:
+for i in range(5):
+    print(" " * (5 - i) + "*" * (2 * i - 1))
 
-	def __init__(self, name, grade, level, skillname):
-		self.name = name
-		self.grade = grade
-		self.level = level
-		self.skillname = skillname
-
-	def info(self):
-		print(f'이름 :  {self.name}')
-		print(f'등급 : {self.grade}')
-		print(f'레벨 : {self.level}')
-		print(f'스킬 이름 : {self.skillname}')
-		print()
-
-
-kingdom1 = cookie("용감한 쿠키", "COMMON", 18, "공격")
-kingdom2 = cookie("당근맛 쿠키", "RARE", 35, "치명타 공격")
-kingdom3 = cookie("라떼맛 쿠키", "EPIC", 44, "침묵")
-kingdom4 = cookie("퓨어바닐라 쿠키", "ANCIENT", 56, "회복")
-
-kingdom1.info()
-kingdom2.info()
-kingdom3.info()
-kingdom4.info()
-
-
-
-
-
+for j in range(5, 0, -1): #5 4 3 2 1
+    print(" " * (5 - j) + "*" * (2 * j - 1))
