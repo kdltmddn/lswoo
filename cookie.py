67e8d556bfd99d616bcf013a12bd97c89ceb0d67
kdltmddn <82090311+kdltmddn@users.noreply.github.com>
2021-05-26 18:35:50 +0900

    Update and rename test.py to cookie.py

---
+++ cookie.py
@@ -0,0 +1,25 @@
+class cookie:
+
+	def __init__(self, name, grade, level, skillname):
+		self.name = namehttps://github.com/kdltmddn/lswoo/blob/main/test.py
+		self.grade = grade
+		self.level = level
+		self.skillname = skillname
+
+	def info(self):
+		print(f'이름 :  {self.name}')
+		print(f'등급 : {self.grade}')
+		print(f'레벨 : {self.level}')
+		print(f'스킬 이름 : {self.skillname}')
+		print()
+
+
+kingdom1 = cookie("용감한 쿠키", "COMMON", 18, "공격")
+kingdom2 = cookie("당근맛 쿠키", "RARE", 35, "치명타 공격")
+kingdom3 = cookie("라떼맛 쿠키", "EPIC", 44, "침묵")
+kingdom4 = cookie("퓨어바닐라 쿠키", "ANCIENT", 56, "회복")
+
+kingdom1.info()
+kingdom2.info()
+kingdom3.info()
+kingdom4.info()
